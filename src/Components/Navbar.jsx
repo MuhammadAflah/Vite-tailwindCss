9f8fe6f81a47a3decc6f51ce4db84aeb6678c9fa
Muhammad Aflah <muhammadaflah786@gmail.com>
2023-06-03 09:02:54 +0530

    updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -19,15 +19,26 @@ const Navbar = () => {
         <li className='p-4 hover:border-b-2 border-green-500'>Contact</li>
       </ul>
       <div onClick={handleNav} className='block md:hidden'>
-          {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
+        {/* {nav ? <AiOutlineClose className='hover:transform rotate-180' size={20}/> : <AiOutlineMenu size={20} />} */}
+        {nav ? (
+  <AiOutlineClose
+    className='transform hover:rotate-180'
+    size={20}
+  />
+) : (
+  <AiOutlineMenu
+    className='transform hover:rotate-180'
+    size={20}
+  />
+)}
       </div>
       <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : ' fixed left-[-100%]'}>
         <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4 mt-7'>REACT.</h1>
-          <li className='p-4 border-b border-gray-600'>Home</li>
-          <li className='p-4 border-b border-gray-600'>Company</li>
-          <li className='p-4 border-b border-gray-600'>Resources</li>
-          <li className='p-4 border-b border-gray-600'>About</li>
-          <li className='p-4'>Contact</li>
+        <li className='p-4 border-b border-gray-600'>Home</li>
+        <li className='p-4 border-b border-gray-600'>Company</li>
+        <li className='p-4 border-b border-gray-600'>Resources</li>
+        <li className='p-4 border-b border-gray-600'>About</li>
+        <li className='p-4'>Contact</li>
       </ul>
     </div>
   );
